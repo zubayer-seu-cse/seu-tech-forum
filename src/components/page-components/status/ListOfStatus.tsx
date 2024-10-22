dff935721f34e27c7ab98bbbdcfdcaa0081dcb11
Md. Mahamudul Hasan Zubayer <159685856+zubayer-seu-cse@users.noreply.github.com>
2024-10-22 08:42:23 +0600

    Update ListOfStatus.tsx

--- src/components/page-components/status/ListOfStatus.tsx
+++ src/components/page-components/status/ListOfStatus.tsx
@@ -37,7 +37,7 @@ export default function ListOfStatus() {
                         <div className="p-4 bg-white rounded-lg dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
                             <h2 className="mb-3 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">Powering innovation & trust at 200,000+ companies worldwide</h2>
                             <p className="mb-3 text-gray-500 dark:text-gray-400">Empower Developers, IT Ops, and business teams to collaborate at high velocity. Respond to changes and deliver great customer and employee service experiences fast.</p>
-                            <Link href="/questions/details/1" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                            <Link href="/feed/details/1" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
                                 Read more
                                 <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
@@ -59,7 +59,7 @@ export default function ListOfStatus() {
                         <div className="p-4 bg-white rounded-lg dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
                             <h2 className="mb-3 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">Powering innovation & trust at 200,000+ companies worldwide</h2>
                             <p className="mb-3 text-gray-500 dark:text-gray-400">Empower Developers, IT Ops, and business teams to collaborate at high velocity. Respond to changes and deliver great customer and employee service experiences fast.</p>
-                            <Link href="/questions/details/1" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                            <Link href="/feed/details/1" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
                                 Read more
                                 <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
@@ -81,7 +81,7 @@ export default function ListOfStatus() {
                         <div className="p-4 bg-white rounded-lg dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
                             <h2 className="mb-3 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">Powering innovation & trust at 200,000+ companies worldwide</h2>
                             <p className="mb-3 text-gray-500 dark:text-gray-400">Empower Developers, IT Ops, and business teams to collaborate at high velocity. Respond to changes and deliver great customer and employee service experiences fast.</p>
-                            <Link href="/questions/details/1" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                            <Link href="/feed/details/1" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
                                 Read more
                                 <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
