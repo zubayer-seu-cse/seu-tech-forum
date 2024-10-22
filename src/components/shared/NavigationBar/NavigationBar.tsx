bbfd169e319e84761938e853127e8f9b5618de4f
Md. Mahamudul Hasan Zubayer <159685856+zubayer-seu-cse@users.noreply.github.com>
2024-10-22 08:53:30 +0600

    Update NavigationBar.tsx

--- src/components/shared/NavigationBar/NavigationBar.tsx
+++ src/components/shared/NavigationBar/NavigationBar.tsx
@@ -11,7 +11,7 @@ export default function NavigationBar() {
 
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0">
-            <div className="max-w-[90%] lg:max-w-[90%] flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="max-w-[100%] lg:max-w-[90%] flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SEU Tech Forum</span>
