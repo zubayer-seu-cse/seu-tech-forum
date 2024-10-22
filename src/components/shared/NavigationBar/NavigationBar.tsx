78ea7bd679ff34b2efbc206ebe85b034720fd735
Md. Mahamudul Hasan Zubayer <159685856+zubayer-seu-cse@users.noreply.github.com>
2024-10-22 09:12:31 +0600

    Update NavigationBar.tsx

--- src/components/shared/NavigationBar/NavigationBar.tsx
+++ src/components/shared/NavigationBar/NavigationBar.tsx
@@ -105,6 +105,9 @@ export default function NavigationBar() {
                         <li>
                             <Link href="/questions/ask-a-question" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Ask Question</Link>
                         </li>
+                        <li>
+                            <Link href="/users" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Users</Link>
+                        </li>
                     </ul>
                 </div>
             </div>
